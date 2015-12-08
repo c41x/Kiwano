dcae9d9e45b00a342f605ca5582c933e754c09b7
c41x <jakubduracz@gmail.com>
2015-12-08 15:32:22 +0100

    + new default interface

--- data/init.lisp
+++ data/init.lisp
@@ -1,53 +1,109 @@
-(create-playlist 'p1)
-(create-playlist 'p2)
-(create-layout 'l1 t)
-(layout-add-component 'l1 'p1 -0.1 -0.9 -0.5)
-(layout-add-splitter 'l1)
-(layout-add-component 'l1 'p2 -0.1 -0.9 -0.5)
-(create-layout 'l2 nil)
-(create-interpreter 'int1)
-(layout-add-component 'l2 'int1 50.0 100.0 50.0)
-(layout-add-splitter 'l2)
-(layout-add-component 'l2 'l1 -0.1 -1.0 -0.9)
+;; (create-playlist 'p1)
+;; (create-playlist 'p2)
+;; (create-layout 'l1 t)
+;; (layout-add-component 'l1 'p1 -0.1 -0.9 -0.5)
+;; (layout-add-splitter 'l1)
+;; (layout-add-component 'l1 'p2 -0.1 -0.9 -0.5)
+;; (create-layout 'l2 nil)
+;; (create-interpreter 'int1)
+;; (layout-add-component 'l2 'int1 50.0 100.0 50.0)
+;; (layout-add-splitter 'l2)
+;; (layout-add-component 'l2 'l1 -0.1 -1.0 -0.9)
+;;
+;; (set-main-component 'l2)
+;;
+;; (create-tabs 'tab 'bottom)
+;; (create-playlist 'plpl)
+;;
+;; (defun on-playlist-click (item-str)
+;;   (playback-set-file item-str)
+;;   (slider-range 'slider 0.0 (playback-length))
+;;   (playback-start) )
+;;
+;; (defun on-slider-up (time)
+;;   (playback-seek time))
+;;
+;; (defun on-update-slider ()
+;;   (slider-value 'slider (playback-get-pos)))
+;;
+;; (create-timer 'update-slider 'on-update-slider)
+;; (start-timer 'update-slider 500) ;; start-stop?
+;;
+;; (bind-mouse-double-click 'plpl 'on-playlist-click '(selected-row))
+;;
+;; (layout-add-component 'l2 'tab 300.0 300.0 300.0)
+;; (defvar cl-red |1.0 0.0 0.0 1.0|)
+;; (tabs-add-component 'tab 'plpl "First tab" cl-red)
+;; (create-audio-settings 'sss)
+;; (tabs-add-component 'tab 'sss "Audio Settings" |0.0 0.8 0.0 0.5|)
+;; (defun on-play-clicked ()
+;;   (layout-remove-splitter 'l1 0)
+;;   (refresh-interface))
+;;
+;; ;; (defun playback-changed ()
+;; ;;   (if (not (playback-is-playing))
+;; ;;       (on-play-clicked)))
+;; ;; (bind-playback 'playback-changed)
+;;
+;; (tabs-add-component 'tab (create-text-button 'playb "Play" "Plays selected track") "Playback API" |0.0 0.1 0.5 0.9|)
+;; (tabs-add-component 'tab (create-slider 'slider) "Slider" |0.0 0.5 0.5 0.9|)
+;; (bind-mouse-click 'playb 'on-play-clicked)
+;; (bind-slider-drag-end 'slider 'on-slider-up '(slider-value))
+;;
+;; (refresh-interface)
 
-(set-main-component 'l2)
+(defun create-playlist-tabs ()
+  (create-tabs 'playlist-tabs 'top)
+  (create-playlist 'playlist)
+  (tabs-add-component 'playlist-tabs 'playlist "all" |0.5 0.5 0.5 0.9|)
+  'playlist-tabs)
 
-(create-tabs 'tab 'bottom)
-(create-playlist 'plpl)
+(defun create-button-page ()
+  (create-layout 'l-buttons t)
+  (create-text-button 'b-play ">" "Play")
+  (create-text-button 'b-pause "||" "Pause")
+  (create-text-button 'b-stop "[ ]" "Stop playback")
+  (create-text-button 'b-options "Options" "Audio options")
+  (create-text-button 'b-interpreter "Interpreter" "GLISP Interpreter")
+  (create-slider 'sl-seek)
+  (layout-add-component 'l-buttons 'b-play 30.0 100.0 -1.0)
+  (layout-add-component 'l-buttons 'b-pause 30.0 100.0 -1.0)
+  (layout-add-component 'l-buttons 'b-stop 30.0 100.0 -1.0)
+  (layout-add-component 'l-buttons 'b-options 80.0 100.0 -1.0)
+  (layout-add-component 'l-buttons 'b-interpreter 80.0 100.0 -1.0)
+  (layout-add-component 'l-buttons 'sl-seek -1.0 -1.0 -1.0)
+  'l-buttons)
 
-(defun on-playlist-click (item-str)
-  (playback-set-file item-str)
-  (slider-range 'slider 0.0 (playback-length))
-  (playback-start) )
+(create-layout 'l-main nil)
+(layout-add-component 'l-main (create-button-page) 20.0 20.0 20.0)
+(layout-add-component 'l-main (create-playlist-tabs) -1.0 -1.0 -1.0)
 
-(defun on-slider-up (time)
-  (playback-seek time))
+;; callbacks
+(defun get-audio-settings ()
+  (if (has-component 'audio-settings)
+      'audio-settings
+    (create-audio-settings 'audio-settings)))
 
-(defun on-update-slider ()
-  (slider-value 'slider (playback-get-pos)))
+(defun spawn-audio-options ()
+  (if (not (tabs-index 'playlist-tabs "Audio Options"))
+      (progn
+	(tabs-add-component 'playlist-tabs (get-audio-settings) "Audio Options" |0.9 0.5 0.5 0.9|)
+	(tabs-index 'playlist-tabs "Audio Options"))))
 
-(create-timer 'update-slider 'on-update-slider)
-(start-timer 'update-slider 500) ;; start-stop?
+(defun get-interpreter ()
+  (if (has-component 'interpreter)
+      'interpreter
+    (create-interpreter 'interpreter)))
 
-(bind-mouse-double-click 'plpl 'on-playlist-click '(selected-row))
+(defun spawn-interpreter ()
+  (if (not (tabs-index 'playlist-tabs "Interpreter"))
+      (progn
+	(tabs-add-component 'playlist-tabs (get-interpreter) "Interpreter" |0.5 0.9 0.5 0.9|)
+	(tabs-index 'playlist-tabs "Interpreter"))))
 
-(layout-add-component 'l2 'tab 300.0 300.0 300.0)
-(defvar cl-red |1.0 0.0 0.0 1.0|)
-(tabs-add-component 'tab 'plpl "First tab" cl-red)
-(create-audio-settings 'sss)
-(tabs-add-component 'tab 'sss "Audio Settings" |0.0 0.8 0.0 0.5|)
-(defun on-play-clicked ()
-  (layout-remove-splitter 'l1 0)
-  (refresh-interface))
+;; bindings
+(bind-mouse-click 'b-options 'spawn-audio-options)
+(bind-mouse-click 'b-interpreter 'spawn-interpreter)
 
-;; (defun playback-changed ()
-;;   (if (= (playback-is-playing) nil)
-;;       (on-play-clicked)))
-;; (bind-playback 'playback-changed)
-
-(tabs-add-component 'tab (create-text-button 'playb "Play" "Plays selected track") "Playback API" |0.0 0.1 0.5 0.9|)
-(tabs-add-component 'tab (create-slider 'slider) "Slider" |0.0 0.5 0.5 0.9|)
-(bind-mouse-click 'playb 'on-play-clicked)
-(bind-slider-drag-end 'slider 'on-slider-up '(slider-value))
-
+(set-main-component 'l-main)
 (refresh-interface)
