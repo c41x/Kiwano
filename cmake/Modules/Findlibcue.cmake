29a25dc204fbcd36a96622ccee5df12b69150f81
c41x <jakubduracz@gmail.com>
2016-01-04 14:08:04 +0100

    + libcue, search on windows

--- cmake/Modules/Findlibcue.cmake
+++ cmake/Modules/Findlibcue.cmake
@@ -8,21 +8,23 @@ if (NOT WIN32)
     pkg_check_modules(PC_LIBCUE libcue)
 endif (NOT WIN32)
 
-find_path(LIBCUE_INCLUDE_DIR NAMES libcue/libcue.h
+find_path(LIBCUE_INCLUDE_DIR NAMES libcue/libcue.h libcue.h
     PATHS
     ${PC_LIBCUE_INCLUDEDIR}
     ${PC_LIBCUE_INCLUDE_DIRS}
+    $ENV{PROGRAMFILES}/libcue/include
 )
 
 message(STATUS "libcue/includes: ${LIBCUE_INCLUDE_DIR}")
 
-find_library(LIBCUE_LIBRARIES NAMES cue
+find_library(LIBCUE_LIBRARIES NAMES cue libcue
     PATHS
     ${PC_LIBCUE_LIBDIR}
     ${PC_LIBCUE_LIBRARY_DIRS}
+    $ENV{PROGRAMFILES}/libcue/lib
 )
 
-message(STATUS "libcue/libraries: ${LIBCUE_LIBRARY}")
+message(STATUS "libcue/libraries: ${LIBCUE_LIBRARIES}")
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(libcue DEFAULT_MSG LIBCUE_INCLUDE_DIR LIBCUE_LIBRARIES)
