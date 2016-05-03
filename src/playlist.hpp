718eec71772cbdd0fc8ae58d101ec6e7ea0056e1
c41x <jakubduracz@gmail.com>
2016-05-03 13:19:47 +0200

    + specialization for streaming seekRange struct

--- src/playlist.hpp
+++ src/playlist.hpp
@@ -16,11 +16,23 @@ extern "C" {
 }
 
 struct seekRange {
-	int start;
-	int end;
+	int32 start;
+	int32 end;
 	seekRange() : start(0), end(0) {}
-	seekRange(int _start, int _end) : start(_start), end(_end) {}
+	seekRange(int32 _start, int32 _end) : start(_start), end(_end) {}
 };
+
+namespace granite { namespace base {
+// overloads for writing seekRange
+template <> inline size_t stream::read(seekRange &out) {
+	return read<int32>(out.start) + read<int32>(out.end);
+}
+
+template <> inline void stream::write(const seekRange &in) {
+	write<int32>(in.start);
+	write<int32>(in.end);
+}
+}}
 
 class playlist : public Component, public FileDragAndDropTarget {
     struct playlistModel : public TableListBoxModel {
